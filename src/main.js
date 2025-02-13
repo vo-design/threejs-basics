89d39fe889c104eaae1d975db0bf641310bce3ba
Volodymyr O <hello@vdigital.design>
2025-02-13 17:32:46 +0200

    Transform objects

--- src/main.js
+++ src/main.js
@@ -1,16 +1,43 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
 
-//Canvas
-const canvas = document.querySelector('canvas.webjl');
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
-//Scene
-const scene = new THREE.Scene();
+// Scene
+const scene = new THREE.Scene()
 
-//Object
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: 0xff0000});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+//Axes Helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+
+//Objects
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+cube1.position.x = -1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.x = 0
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
 
 //Sizes
 const sizes = {
@@ -19,15 +46,14 @@ const sizes = {
 }
 
 //Camera
-const camera = new THREE.PerspectiveCamera (75, sizes.width / sizes.height);
-camera.position.z = 3;
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+// camera.lookAt(new THREE.Vector3(0, - 1, 0))
+scene.add(camera)
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
-
 renderer.setSize(sizes.width, sizes.height);
-
 renderer.render(scene, camera)
