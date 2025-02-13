441a9209ce2fe01d7a6a42832be3ad67bff07ae7
Volodymyr O <hello@vdigital.design>
2025-02-13 19:38:34 +0200

    Using Clock

--- src/main.js
+++ src/main.js
@@ -30,18 +30,18 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 
-let time = Date.now();
+const clock = new THREE.Clock();
 
 //Animations
 const tick = () => {
-    //Time
-    const currentTime = Date.now();
-    const deltaTime = currentTime - time;
-    time = currentTime;
 
+    //Clock
+    const elapsedTime = clock.getElapsedTime();
 
     // Update objects
-    mesh.rotation.y += 0.001 * deltaTime;
+    mesh.position.y = Math.sin(elapsedTime);
+    mesh.position.x = Math.cos(elapsedTime);
+    camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
