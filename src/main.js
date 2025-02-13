c9bafe8ea988efaafed6d254c54f39289aa8d0a7
Volodymyr O <hello@vdigital.design>
2025-02-13 19:31:05 +0200

    Using requestAnimationFrame

--- src/main.js
+++ src/main.js
@@ -6,54 +6,48 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-//Axes Helper
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-
-//Objects
-const group = new THREE.Group()
-group.scale.y = 2
-group.rotation.y = 0.2
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0xff0000})
-)
-cube1.position.x = -1.5
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0x00ff00})
-)
-cube2.position.x = 0
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0x0000ff})
-)
-cube3.position.x = 1.5
-group.add(cube3)
-
-
-//Sizes
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-//Camera
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-// camera.lookAt(new THREE.Vector3(0, - 1, 0))
 scene.add(camera)
 
-//Renderer
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera)
+})
+renderer.setSize(sizes.width, sizes.height)
+
+
+let time = Date.now();
+
+//Animations
+const tick = () => {
+    //Time
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+
+
+    // Update objects
+    mesh.rotation.y += 0.001 * deltaTime;
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+};
+
+tick();
