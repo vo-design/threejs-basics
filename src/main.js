e3892602f01f96b27a35165841387f8c1d9a6371
Volodymyr O <hello@vdigital.design>
2025-02-14 19:35:06 +0200

    Fill in the viewport

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
 // Cursor
