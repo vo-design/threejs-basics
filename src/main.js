b364c4fad7549d7c627cc1369f2e53914c2763f6
Volodymyr O <hello@vdigital.design>
2025-02-14 19:51:29 +0200

    Handle Pixel Ratio

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ window.addEventListener('resize', ()=> {
 
     //Update renderer
     renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 // Cursor
@@ -63,6 +64,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Animate
 const clock = new THREE.Clock()
