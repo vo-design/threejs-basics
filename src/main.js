898193cc72e6021ca6151fc31e3fa19e6b59a72a
Volodymyr Onishchenko <hello@vdigital.design>
2025-02-16 10:51:39 +0200

    MeshLambertMaterial

--- src/main.js
+++ src/main.js
@@ -55,7 +55,22 @@ matcapTexture.colorSpace = THREE.SRGBColorSpace
 // material.matcap = matcapTexture
 
 // MeshDepthMaterial
-const material = new THREE.MeshDepthMaterial()
+// const material = new THREE.MeshDepthMaterial()
+
+// MeshLambertMaterial
+const material = new THREE.MeshLambertMaterial()
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 30)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 
 const sphere = new THREE.Mesh(
